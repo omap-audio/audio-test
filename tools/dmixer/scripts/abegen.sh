1742f2a384750d3f547bc130da6e54b634a98440
Liam Girdwood <lrg@ti.com>
2012-08-21 10:12:32 +0100

    dmixer: fw: use new path for FW

--- tools/dmixer/scripts/abegen.sh
+++ tools/dmixer/scripts/abegen.sh
@@ -2,6 +2,9 @@ LOCAL_SO_PATH=./plugins/omap4plus
 COEFF_PATH=$LOCAL_SO_PATH/coeffs/.libs
 DAPM_PATH=$LOCAL_SO_PATH/dapm/.libs
 LINUX_PATH=~/source/linux.git
+ABE_GEN_PATH=../abe-gen
+
+#	-vfw $LINUX_PATH/firmware/omap4_abe
 
 ./src/socfw omap4_abe_new \
 	-p $COEFF_PATH/dl1.so \
@@ -11,4 +14,4 @@ LINUX_PATH=~/source/linux.git
 	-p $COEFF_PATH/dmic.so \
 	-p $COEFF_PATH/amic.so \
 	-p $DAPM_PATH/abe-dl1dl2-twl6040.so \
-	-vfw $LINUX_PATH/firmware/omap4_abe
+	-vfw $ABE_GEN_PATH/omap_abe_fw
